75849b7a48ddf86228808fb24b208fe79aa0372f
Sebastian Aguinaga Velásquez <saguinagavelasquez@gmail.com>
2023-10-22 20:42:46 -0500

    Politica_Datos

--- aplicacion.py
+++ aplicacion.py
@@ -155,14 +155,28 @@ else:
         new_password = st.text_input("Nueva Contraseña:", type = "password")
         confirm_password = st.text_input("Confirmar contraseña:", type = "password")
 
-        #Boton de registro de usuario
-        if st.button("Registrarse"):
-            registration_successful, message = registrar_usuario(new_username, new_password,first_name,
-                                                                last_name, email, confirm_password)
+        # Crear dos columnas para los botones
+        col1, col2 = st.columns(2)
+        # Casilla de verificación para aceptar la política de datos personales
+        aceptar_politica = st.checkbox("Acepta la política de datos personales")
+
+        # Botón de registro de usuario en la primera columna
+        if col1.button("Registrarse") and aceptar_politica:
+            registration_successful, message = registrar_usuario(new_username, new_password)
             if registration_successful:
                 st.success(message)
             else:
                 st.error(message)
+
+        if not aceptar_politica:
+            st.warning("Por favor, acepta la política de datos personales antes de registrarte.")
+
+        # Botón para abrir la ventana emergente en la segunda columna
+        if col2.button("Ver Política de Tratamiento de Datos"):
+            with open("politica_datos.txt", "r") as archivo:
+                politica = archivo.read()
+                with st.expander("Política de Tratamiento de Datos"):
+                    st.write(politica)
     elif menu_option == "Salir":
         st.balloons()
         st.stop()
