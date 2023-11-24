12938b64b56235293cb8b44ac8f6693352a4ae97
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-11-24 13:22:25 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -473,8 +473,9 @@ if get_current_user() is not None:
         crear_grafico_barras_categorias()
 
     if menu_option == "Eliminar gasto ó ingreso":
-        gas_ing = st.text_input = ("Ingrese la 'Key' del gasto o del ingreso")
-        if st.button("Eliminar Gato o ingreso"):
+        st.header("Seccion Para eliminacion de datos")
+        gas_ing = st.text_input = ("Ingrese la 'Key' del gasto o del ingreso:")
+        if st.button("Eliminar Gasto o ingreso"):
             esta = db_data.fetch({"key":gas_ing})
             if esta.count>0:
                 db_data.delete(gas_ing)
