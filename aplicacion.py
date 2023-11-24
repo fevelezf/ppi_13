04d5b7e97347f32dac902c4e4f2d1903541c4158
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-11-24 13:58:56 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -546,22 +546,26 @@ if get_current_user() is not None:
         descargar_datos_excel(df)
 
     elif menu_option == "Actualizar Datos":
-        with st.expander("contraseña"):
+        st.header("Seccion De actualizacion de datos")
+        with st.expander("Contraseña"):
             us = st.text_input("Nickname:")
             ps = st.text_input("Contraseña:", type="password")
             ps_new = st.text_input("Nueva Contraseña:", type="password")
+            ps_new_conf = st.text_input("Confirmar Nueva Contraseña:", type="password")
+            if ps_new == ps_new_conf:
+                if st.button("Cambiar contraseña"):
+                    login_successful, message = verificar_credenciales(us, ps)
+                    if login_successful:
+                        us_s = db_users.fetch({"username":us})
+                        itm = us_s.items[0]
+                        llave = itm.get("key")
+                        db_users.update({"password":ps_new},key=llave)
+                        st.success("Contraseña cambiada con exito")
 
-            if st.button("Cambiar contraseña"):
-                login_successful, message = verificar_credenciales(us, ps)
-                if login_successful:
-                    us_s = db_users.fetch({"username":us})
-                    itm = us_s.items[0]
-                    llave = itm.get("key")
-                    db_users.update({"password":ps_new},key=llave)
-                    st.success("Contraseña cambiada con exito")
-
-                else:
-                    st.warning("Credenciales incorrectas")
+                    else:
+                        st.warning("Credenciales incorrectas")
+            else:
+                st.warning("Las Contraseñas no coinciden")
 
 
 
